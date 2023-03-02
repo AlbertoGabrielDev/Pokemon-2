8142faf6242a65b3daff1c87ec301c5e3717628f
brasi16 <albertobiel00@hotmail.com>
2023-03-01 22:15:33 -0300

    att js

--- src/componets/Pokedex.jsx
+++ src/componets/Pokedex.jsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
-
+import './pokedex.css';
 const Pokedex = () => {
-    const { pokemon,abilities } = useContext(GlobalContext);
+    const { pokemon, abilities } = useContext(GlobalContext);
 
     var h0 = "";
     var h1 = "";
@@ -13,27 +13,40 @@ const Pokedex = () => {
     return (
         <>
             <div>
-                {/* {
-                    pokemon && pokemon.map((e) => {
-                        return (
-                            <>
-                                <div key={e.name}>{e.name}</div>
-                               
-                            </>
-                        )
-                    })
-                } */}
-
-                <h1>Lista</h1>
+                
                 {
-                   pokemon && pokemon.map((pokemons)=>(
+                pokemon && pokemon.map((pokemons) => {
+                    return (
                         <div key={pokemons.name}>
-                            <h2>{pokemons.name}</h2>
-                            <p>Habilidades: {pokemons.types.join(", ")}</p>
+                            <div className="card">
+                                <div className="imagem">
+                                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt={`${pokemon.name}`} />
+                                </div>
+                                <span className="nome">{pokemons.name}</span>
+
+                                <p id='habilidade-0'>{pokemons.abilities.join(", ")}</p>
+                                <p id="tipo0">{pokemons.types.join(", ")}</p>
+                                
+                                    {
+                                            pokemons.abilities.forEach((poke, index) => {
+                                                if (index == 0) {
+                                                    h0 = poke.ability.name;
+
+                                                } if (index == 1) {
+                                                    h1 = poke.ability.name
+                                                }
+
+                                            })
+                                        }
+                            </div>
+                            
                         </div>
                         
-                    ))
+                    )
+                })
+                
                 }
+               
 
             </div>
         </>
