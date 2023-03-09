dfec5b36ce170669b71b04c66aeb7b097155b639
brasi16 <albertobiel00@hotmail.com>
2023-03-08 22:53:58 -0300

    att js

--- src/componets/Pokedex.jsx
+++ src/componets/Pokedex.jsx
@@ -1,38 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import './pokedex.css';
+
 const Pokedex = () => {
-    const { pokemon, abilities } = useContext(GlobalContext);
+    const { pokemon } = useContext(GlobalContext);
+    const [h0, setH0] = useState("");
+    const [h1, setH1] = useState("");
+    const [t0, setT0] = useState("");
+    const [t1, setT1] = useState("");
 
-    var h0 = "";
-    var h1 = "";
+    const pokemonList = [];
 
-    var t0 = "";
-    var t1 = "";
-    //console.log(pokemon.name)
-    return (
-        <>
-        <div>
-            {pokemon && pokemon.map((pokemons,index) => {
-                return (
-                    <div key={pokemons.name}>
-                        <div className="card">
-                            <div className="imagem">
-                                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index +1 }.svg`} alt={`${pokemons.name}`} />
-                            </div>
-                            <span className="nome">{pokemons.name}</span>
-                            {<p id='habilidade-0'>{pokemons.abilities[0]}</p>}
-                            <p id='habilidade-1'>{pokemons.abilities[1]}</p>
-                            <p id="tipo0">{pokemons.types[0]}</p>
-                            <p id="tipo1">{pokemons.types[1] }</p>
+    if (pokemon) {
+        pokemon.forEach((pokemons, index) => {
+            pokemonList.push(
+                <div key={pokemons.name}>
+                    <div className="card">
+                        <div className="imagem">
+                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index +1 }.svg`} alt={`${pokemons.name}`} />
                         </div>
+                        <span className="nome">{pokemons.name}</span>
+                        {<p id='habilidade-0'>{pokemons.abilities[0]}</p>}
+                        <p id='habilidade-1'>{pokemons.abilities[1]}</p>
+                        <p id="tipo0">{pokemons.types[0]}</p>
+                        <p id="tipo1">{pokemons.types[1]}</p>
                     </div>
-                )
-            })}
+                </div>
+            );
+        });
+    }
+
+    return (
+        <div>
+            {pokemonList}
         </div>
-    </>
     );
 }
-
 
 export default Pokedex;
