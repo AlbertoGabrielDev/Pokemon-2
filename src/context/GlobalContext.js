ae36934e18320255c4a3bf3433fde53d72cb60fc
brasi16 <albertobiel00@hotmail.com>
2023-03-12 12:11:47 -0300

    att js

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -8,17 +8,21 @@ import api from "../serve/api";
 
 export const GlobalContext = createContext();
 
-export const GlobalProvaider = ({
-    children
-}) => {
+export const GlobalProvaider = ({children}) => {
     const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
     const [pokemon, setPokemon] = useState(null);
     const [error, setError] = useState(false);
+    const [nextUrl, setNextUrl] = useState();
+    const [prevUrl, setPrevUrl] = useState();
 
+    console.log(nextUrl)
     useEffect(() => {
         const getPokemonList = async () => {
             try {
                 const response = await axios.get(url)
+                setNextUrl(response.data.next);
+                setPrevUrl(response.data.prev);
+
                 const results = response.data.results
                 const pokemonData = await Promise.all(results.map(async (result) => {
                     const pokemonResponse = await axios.get(result.url);
@@ -38,17 +42,26 @@ export const GlobalProvaider = ({
 
 
 
-    return ( <GlobalContext.Provider value = {
+    return (
+    <GlobalContext.Provider value = {
             {
                 pokemon,
                 error,
                 setError,
-                setPokemon
-                
+                setPokemon,
+               
             }
-        } > { children} </GlobalContext.Provider>
+        }>{ children}
+         {
+            <button onClick={() => {
+                
+                setUrl(nextUrl)
+            }}>Pagina Seguinte</button>
+        }
 
+        </GlobalContext.Provider>
 
+       
     )
 
 }
