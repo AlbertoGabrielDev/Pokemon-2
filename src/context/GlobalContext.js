d3d42b9f1fba3c197df2a922442cc79be26cb780
brasi16 <albertobiel00@hotmail.com>
2023-03-26 22:06:47 -0300

    att js e estudar selectType

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -10,6 +10,7 @@ export const GlobalContext = createContext();
 
 export const GlobalProvaider = ({children}) => {
     const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+    const [types, setTypes] = useState("https://pokeapi.co/api/v2/type");
     const [pokemon, setPokemon] = useState(null);
     const [error, setError] = useState(false);
     const [nextUrl, setNextUrl] = useState();
@@ -19,18 +20,19 @@ export const GlobalProvaider = ({children}) => {
     useEffect(() => {
         const getPokemonList = async () => {
             try {
-                const response = await axios.get(url)
+                const response = await axios.get(url,types)
                 setNextUrl(response.data.next);
                 setPrevUrl(response.data.previous);
 
                 const results = response.data.results
                 const pokemonData = await Promise.all(results.map(async (result) => {
-                    const pokemonResponse = await axios.get(result.url);
+                    const pokemonResponse = await axios.get(result.url, result.types);
                     return {
                         name: pokemonResponse.data.name,
                         abilities: pokemonResponse.data.abilities.map((ability) => ability.ability.name),
                         types: pokemonResponse.data.types.map((type) => type.type.name),
                         stats: pokemonResponse.data.stats.map((status)=> status.base_stat),
+                        // selectTypes: pokemonResponse.data.map(()=>),
                         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonResponse.data.id}.svg`,
                     }
                 }))
